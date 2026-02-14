c8c9ed44eb492de7080401bfb4fcc5ea7ed155e8
Kyoya Sugiyama <sugi.kyoya@gmail.com>
2026-02-15 00:52:33 +0900

    add shell script

---
+++ main_files/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#============ Slurm Options ===========
+#SBATCH -p gr20001a
+#SBATCH -t 00:10:00
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+#SBATCH --rsc p=256:t=1:c=1:m=2G
+
+# エラーが出たら即終了
+set -e
+
+#============ Shell Script ============
+# 1. 環境初期化
+. /usr/share/Modules/init/bash
+
+module use /opt/system/app/env/intel/Compiler
+module use /opt/system/app/env/intel/MPI
+module purge
+module load intel
+module load intelmpi
+
+echo "Compiler:"
+mpiicpx --version
+echo "Current dir:"
+pwd
+
+# 2. コンパイル（C++20対応 & includeパス追加）
+mpiicpx -O3 -std=c++20 \
+-Wl,-rpath=${I_MPI_ROOT}/libfabric/lib \
+two_stream_instability_super.cpp -o vlasov
+
+echo "Compile finished."
+
+# 高速通信を明示
+export I_MPI_FABRICS=shm:ofi
+
+# OFIプロバイダ自動選択
+unset I_MPI_OFI_PROVIDER
+
+# デバッグ表示（確認用）
+#export I_MPI_DEBUG=5
+
+# 3. 実行
+srun --mpi=pmi2 -n 256 ./vlasov
+
